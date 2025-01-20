6eaf073c0afdb513b0de475f3e884dabc6a548a6
ali johar <johar.ali1@gmail.com>
2025-01-20 19:27:20 +0300

    Add tree toc

---
+++ lib/screen/toc/cubit/toc_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:zahra/model/toc_item.dart';
+
+import '../../../repository/json_repository.dart';
+
+part 'toc_state.dart';
+part 'toc_cubit.freezed.dart';
+
+class TocCubit extends Cubit<TocState> {
+
+  TocCubit() : super(const TocState.initial());
+  final JsonRepository _jsonRepository = JsonRepository();
+
+  Future<void> fetchItems(int id) async {
+    try {
+      emit(const TocState.loading());
+      print('id is $id');
+      final items = await _jsonRepository.fetchJsonTocById(id);
+      emit(TocState.loaded(items));
+    } catch (e) {
+      emit(TocState.error(e.toString()));
+    }
+  }
+}
