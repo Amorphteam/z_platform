289e84bc45ccc6a16e67c670639a18852346e8f9
ali johar <johar.ali1@gmail.com>
2025-01-25 10:21:35 +0300

    Handle Search sheet for showing books and selecte the range

--- lib/screen/search/cubit/search_state.dart
+++ lib/screen/search/cubit/search_state.dart
@@ -5,5 +5,6 @@ class SearchState with _$SearchState {
   const factory SearchState.initial() = _Initial;
   const factory SearchState.loading() = _Loading;
   const factory SearchState.loaded({required List<SearchModel> searchResults}) = _Loaded;
+  const factory SearchState.loadedList(List<Book> books) = _LoadedList;
   const factory SearchState.error({required String error}) = _error;
 }
